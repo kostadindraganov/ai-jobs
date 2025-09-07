48bb92759f1399e4425be0052fdf1b5ef2e58039
Kostadin Draganov <kostadindraganov@gmail.com>
2025-09-08 01:07:40 +0300

    Added inngest add user events

--- src/data/env/server.ts
+++ src/data/env/server.ts
@@ -8,8 +8,8 @@ export const env = createEnv({
     DB_HOST: z.string().min(1),
     DB_PORT: z.string().min(1),
     DB_NAME: z.string().min(1),
-    CLERK_SECRET_KEY: z.string().min(1)
-    // CLERK_WEBHOOK_SECRET: z.string().min(1),
+    CLERK_SECRET_KEY: z.string().min(1),
+    CLERK_WEBHOOK_SECRET: z.string().min(1),
     // UPLOADTHING_TOKEN: z.string().min(1),
     // ANTHROPIC_API_KEY: z.string().min(1),
     // GEMINI_API_KEY: z.string().min(1),
