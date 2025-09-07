c25fc1c08332e2ea00d3b9575c2852f52a0f6357
Kostadin Draganov <kostadindraganov@gmail.com>
2025-09-07 16:03:12 +0300

    Added simple UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
-import { SidebarProvider } from "@/components/ui/sidebar";
+import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarProvider, SidebarTrigger} from "@/components/ui/sidebar";
+import { AppSidebarClient } from "./_AppSidebarClient";
 
 export default function HomePage() {
-  return <SidebarProvider>
-    
-  </SidebarProvider>
+  return (
+  <SidebarProvider className="overflow-y-hidden">
+     <AppSidebarClient>
+      <Sidebar collapsible="icon" className="overflow-hidden">
+        <SidebarHeader className="flex-row">
+        <SidebarTrigger />
+        <span className="text-xl text-nowrap">AI jobs</span>
+        </SidebarHeader>
+        <SidebarContent></SidebarContent>
+        <SidebarFooter>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton>Btn</SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarFooter>
+      </Sidebar>
+      <main className="flex-1">children</main>
+    </AppSidebarClient>
+    </SidebarProvider>
+  )
 }
