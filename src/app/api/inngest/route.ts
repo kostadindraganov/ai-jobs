a5b54a200278d99319e7575cd5d87e873d0de8d1
Kostadin Draganov <kostadindraganov@gmail.com>
2025-09-16 00:50:26 +0300

    Added: react-email, resend, email templates

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -9,12 +9,12 @@ import {
   clerkUpdateOrganization,
   clerkUpdateUser,
 } from "@/services/inngest/functions/clerk"
-// import {
-//   prepareDailyOrganizationUserApplicationNotifications,
-//   prepareDailyUserJobListingNotifications,
-//   sendDailyOrganizationUserApplicationEmail,
-//   sendDailyUserJobListingEmail,
-// } from "@/services/inngest/functions/email"
+import {
+  prepareDailyOrganizationUserApplicationNotifications,
+  prepareDailyUserJobListingNotifications,
+  sendDailyOrganizationUserApplicationEmail,
+  sendDailyUserJobListingEmail,
+} from "@/services/inngest/functions/email"
 import { rankApplication } from "@/services/inngest/functions/jobListingApplication"
 import { createAiSummaryOfUploadedResume } from "@/services/inngest/functions/resume"
 import { serve } from "inngest/next"
@@ -32,9 +32,9 @@ export const { GET, POST, PUT } = serve({
     clerkDeleteOrgMembership,
     createAiSummaryOfUploadedResume,
     rankApplication,
-    // prepareDailyUserJobListingNotifications,
-    // sendDailyUserJobListingEmail,
-    // prepareDailyOrganizationUserApplicationNotifications,
-    // sendDailyOrganizationUserApplicationEmail,
+    prepareDailyUserJobListingNotifications,
+    sendDailyUserJobListingEmail,
+    prepareDailyOrganizationUserApplicationNotifications,
+    sendDailyOrganizationUserApplicationEmail,
   ],
 })
